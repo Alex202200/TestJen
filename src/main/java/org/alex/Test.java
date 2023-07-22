dc63809619841cd6b5b7fc8614644a6f8422f4b2
Alexander M <104126037+Alex202200@users.noreply.github.com>
2023-07-22 16:55:01 +0100

    Update Test.java

--- src/main/java/org/alex/Test.java
+++ src/main/java/org/alex/Test.java
@@ -2,6 +2,6 @@ package org.alex;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!!!");
     }
 }
